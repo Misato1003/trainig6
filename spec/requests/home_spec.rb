e6eab10b1298790580117c541af8c2de4962cd74
EC2 Default User <ec2-user@ip-172-31-38-86.ap-northeast-1.compute.internal>
2022-06-22 03:53:56 +0000

    アカウントの編集 表（テーブル)に変更

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /page" do
-    it "returns http success" do
-      get "/home/page"
-      expect(response).to have_http_status(:success)
+    it "ステータスが２００であること（トップページ）" do
+      get root_path
+      expect(response).to have_http_status(200)
     end
   end
-
 end
