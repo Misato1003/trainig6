191d9ca1559296b7568d4432ee85273059928837
EC2 Default User <ec2-user@ip-172-31-38-86.ap-northeast-1.compute.internal>
2022-06-22 04:33:07 +0000

    プロフィールの設定（ユーザー名、画像、一言紹介が登録できる)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # devise(ユーザー名、プロフィール画像、一言紹介が登録できないため)
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # devise　(ユーザー名、プロフィール画像、一言紹介が登録できないため)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :image, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :image, :introduction])
+  end
 end
