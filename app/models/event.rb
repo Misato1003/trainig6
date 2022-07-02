c18be024b99e6403f220132ad5922a9d1b411382
EC2 Default User <ec2-user@ip-172-31-38-86.ap-northeast-1.compute.internal>
2022-07-02 09:18:59 +0000

    eventのバリエーション、エラーメッセージの表示

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
-  
-  #画像のアップロード
+
+  # 画像のアップロード
   mount_uploader :eventimage, ImageUploader
+
+  validates :eventname, presence: true
+  validates :first_day, presence: true
+  validates :last_day, presence: true
+  validates :place, presence: true
+  validates :content, presence: true
 end
